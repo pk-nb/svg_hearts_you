6b06d703096524404b57df12ac9a3cea8b66f2de
Nathanael Beisiegel <pknb.dev@gmail.com>
2014-09-16 16:09:39 -0400

    Tests green and svg_path set in tests

--- lib/svg_hearts_you.rb
+++ lib/svg_hearts_you.rb
@@ -1,12 +1,11 @@
 require 'nokogiri'
+
 begin
   require 'pry'
 rescue LoadError
 end
 
 require 'svg_hearts_you/configuration'
-
-
 
 module SvgHeartsYou
   class << self
@@ -21,15 +20,17 @@ module SvgHeartsYou
     yield(configuration)
   end
 
-  def svg_inline(file)
+  def svg_inline(filename, options={})
+    raise 'svg_path is not set' if self.configuration.nil?
 
+    file = File.read(Configuration.new)
   end
 
   def svg_symbol
-
+    raise 'svg_path is not set' if self.configuration.nil?
   end
 
   def svg_use
-
+    raise 'svg_path is not set' if self.configuration.nil?
   end
 end
--- spec/svg_hearts_you_spec.rb
+++ spec/svg_hearts_you_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe SvgHeartsYou do
+  let(:test_svg_path) { File.join(File.dirname(__FILE__), 'svgs') }
+
   before do
     SvgHeartsYou.configure do |config|
-      config.svg_path = File.dirname(__FILE__)
+      config.svg_path = test_svg_path
     end
   end
 
   it 'is configured to the the current directory' do
-    expect(SvgHeartsYou.configuration.svg_path).to eq(File.dirname(__FILE__))
+    expect(SvgHeartsYou.configuration.svg_path).to eq(test_svg_path)
   end
-
 end
