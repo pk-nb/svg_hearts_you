016bcc481c94de3ccf9fd0461da305f2573cf33f
Nathanael Beisiegel <pknb.dev@gmail.com>
2014-10-01 18:00:02 -0400

    Refactor tests, comment, and edge case support

--- lib/svg_hearts_you.rb
+++ lib/svg_hearts_you.rb
@@ -7,20 +7,18 @@ module SvgHeartsYou
     attr_accessor :configuration
   end
 
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
+  # Allow a persistent configuration object to be set on the module
   def self.configure
     yield(configuration)
   end
 
   def self.svg_inline(filename, options={})
-    raise 'svg_path is not set' if configuration.svg_path.nil?
+    validate_configuration
 
-    path = File.join configuration.svg_path, filename
-    doc = Nokogiri::HTML::DocumentFragment.parse(File.read path)
+    doc = Nokogiri::HTML::DocumentFragment.parse(read_file filename)
     svg = doc.at_css 'svg'
+
+    # Attributes are added after
     options.each do |key, value|
       svg[key.to_s] = value
     end
@@ -29,11 +27,12 @@ module SvgHeartsYou
   end
 
   def self.svg_symbol(filename, options={})
-    raise 'svg_path is not set' if configuration.svg_path.nil?
+    validate_configuration
 
-    path = File.join configuration.svg_path, filename
-    doc = Nokogiri::HTML::DocumentFragment.parse(File.read path)
+    doc = Nokogiri::HTML::DocumentFragment.parse(read_file filename)
 
+    # Create two nodes for the symbol. The original svg tag will turn into
+    # a symbol tag, and the new tag will enclose it.
     original_svg = doc.at_css 'svg'
     new_svg = Nokogiri::XML::Node.new 'svg', doc
 
@@ -41,14 +40,15 @@ module SvgHeartsYou
     original_svg.name = 'symbol'
     original_svg['id'] = filename.chomp('.svg')
 
-    # Move the SVG specific attributes to outer svg
-    ['version', 'xmlns', 'xmlns:xlink'].map do |attr|
+    # Move the SVG specific attributes to outer svg and then wrap
+    %w(version xmlns xmlns:xlink).map do |attr|
       new_svg[attr] = original_svg.delete attr
     end
 
-    # original_svg.parent = new_svg
+    # Wrap old svg-now-symbol in new svg tag
     new_svg.add_child(original_svg)
 
+    # Any extra attributes go on the svg tag
     options.each do |key, value|
       new_svg[key.to_s] = value
     end
@@ -58,13 +58,14 @@ module SvgHeartsYou
 
   def self.svg_use(id, options={})
     doc = Nokogiri::HTML::DocumentFragment.parse <<-YAYUSE
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <use xlink:href="##{id}">
     </svg>
     YAYUSE
 
     svg = doc.at_css 'svg'
 
+    # TODO allow for the title and desc stuff
     options.delete :title
     options.delete :desc
 
@@ -73,5 +74,20 @@ module SvgHeartsYou
     end
 
     svg.to_html
+  end
+
+  private
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.validate_configuration
+    raise 'svg_path is not set' if configuration.svg_path.nil?
+  end
+
+  def self.read_file(filename)
+    path = File.join configuration.svg_path, filename
+    raise "File #{filename} not found" unless File.exists?(path)
+    File.read path
   end
 end
--- spec/svg_hearts_you_spec.rb
+++ spec/svg_hearts_you_spec.rb
@@ -4,6 +4,41 @@ RSpec.describe SvgHeartsYou do
 
   let(:test_svg_path) { File.join(File.dirname(__FILE__), 'svgs') }
 
+  let!(:svg_file)             { 'sapphire.svg' }
+  let!(:nonexistant_svg_file) { 'nope-nope-nope.svg' }
+  let!(:unconfigured_message) { 'svg_path is not set' }
+  let!(:missing_file_message) { "File #{nonexistant_svg_file} not found" }
+
+  shared_examples 'method that needs module configuration' do |method|
+    it 'throws a RuntimeError when not configured' do
+      expect{subject.send(method, svg_file)}.to raise_error(RuntimeError, unconfigured_message)
+    end
+  end
+
+  shared_examples 'method using file' do |method|
+    it 'throws a RuntimeError when file is not found' do
+      expect{subject.send(method, nonexistant_svg_file)}.to raise_error(RuntimeError, missing_file_message)
+    end
+  end
+
+  shared_examples 'svg use' do
+    it 'returns SVG use statement' do
+      svg_content = subject.svg_use 'id'
+      expect(svg_content).to include '<use xlink:href="#id">'
+    end
+
+    it 'adds classes and ids to svg tag' do
+      svg_content = subject.svg_use 'id', id: 'hearts', class: 'love'
+      expect(svg_content).to have_tag('svg', with: { id: 'hearts', class: 'love' })
+    end
+
+    it 'adds any attribute to svg tag' do
+      attributes = { width: '64px', height: '48px', viewport: '0, 0, 12, 24' }
+      svg_content = subject.svg_use 'id', attributes
+      expect(svg_content).to have_tag('svg', with: attributes)
+    end
+  end
+
   describe 'module not configured' do
     describe 'self.configuration' do
       it 'not configured' do
@@ -12,26 +47,15 @@ RSpec.describe SvgHeartsYou do
     end
 
     describe '#svg_inline' do
-      it 'raises RuntimeError' do
-        expect{subject.svg_inline('sapphire.svg')}.to raise_error(RuntimeError)
-      end
+      it_behaves_like 'method that needs module configuration', :svg_inline
     end
 
     describe '#svg_use' do
-      it 'returns SVG use statement' do
-        svg_content = subject.svg_use 'id'
-        expect(svg_content).to include '<use xlink:href="#id">'
-      end
+      include_examples 'svg use'
+    end
 
-      it 'adds classes and ids to svg tag' do
-        svg_content = subject.svg_use 'id', id: 'hearts', class: 'love'
-        expect(svg_content).to include 'id="hearts" class="love"'
-      end
-
-      it 'adds any attribute to svg tag' do
-        svg_content = subject.svg_use 'id', width: '64px', height: '48px', viewport: '0, 0, 12, 24'
-        expect(svg_content).to include 'width="64px" height="48px" viewport="0, 0, 12, 24"'
-      end
+    describe '#svg_symbol' do
+      it_behaves_like 'method that needs module configuration', :svg_symbol
     end
   end
 
@@ -50,26 +74,34 @@ RSpec.describe SvgHeartsYou do
     end
 
     describe '#svg_inline' do
+      it_behaves_like 'method using file', :svg_inline
+
       it 'returns contents of SVG file without XML headers' do
         svg_content = subject.svg_inline 'sapphire.svg'
-        expect(svg_content).to include('<svg')
-        expect(svg_content).to include('sapphire')
+
+        expect(svg_content).to have_tag('svg')
         expect(svg_content).not_to include('<xml')
         expect(svg_content).not_to include('DOCTYPE')
       end
     end
 
-    describe '#svg_symbol' do
-      it 'returns the SVG contents in a symbol' do
-        svg_content = subject.svg_symbol 'sapphire.svg'
+    describe '#svg_use' do
+      include_examples 'svg use'
+    end
 
-        sapphire_svg_attributes = {
-          x: '0',
-          y: '0',
-          width: '64',
-          height: '52',
-          viewbox: '0, 0, 64, 52'
-        }
+    describe '#svg_symbol' do
+      let(:sapphire_svg_attributes) {{
+        x: '0',
+        y: '0',
+        width: '64',
+        height: '52',
+        viewbox: '0, 0, 64, 52'
+      }}
+
+      it_behaves_like 'method using file', :svg_symbol
+
+      it 'returns the SVG contents in a symbol' do
+        svg_content = subject.svg_symbol svg_file
 
         expect(svg_content).not_to have_tag('svg', with: sapphire_svg_attributes)
         expect(svg_content).to have_tag('svg>symbol', with: sapphire_svg_attributes)
